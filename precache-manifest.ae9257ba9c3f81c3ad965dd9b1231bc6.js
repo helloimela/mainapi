be1f26a2925b7e29d27fd6806316252b9d3a23f9
Yudhanira Ela <ela.yudhanira@scania.com>
2020-04-22 21:34:52 +0200

    Updates

---
+++ precache-manifest.ae9257ba9c3f81c3ad965dd9b1231bc6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "387694a35251f4b2e1c415b3762915d4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0512e47d9bb6e587bb15",
+    "url": "/static/css/main.59af7252.chunk.css"
+  },
+  {
+    "revision": "14556e3bea1b3ef68868",
+    "url": "/static/js/2.50eeea2d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.50eeea2d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0512e47d9bb6e587bb15",
+    "url": "/static/js/main.9d2ae2cf.chunk.js"
+  },
+  {
+    "revision": "bc562783c96b73b31703",
+    "url": "/static/js/runtime-main.f3397451.js"
+  }
+]);
