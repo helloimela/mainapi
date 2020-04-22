af7ce16507bf7201588b5b2657ac70c0cd1b7e3c
Ela Yudhanira <ela.yudhanira@scania.com>
2020-04-22 21:52:40 +0200

    Add github pages feature

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
+
 import Game from './components/Game'
 import './index.css';
 
 // ========================================
 
 ReactDOM.render(
-  <Game />,
+  <Router basename={process.env.PUBLIC_URL}><Game /></Router>,
   document.getElementById('root')
 );
