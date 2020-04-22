d42a9df2e1a250d0d74a9f716cce7267079692a7
Yudhanira Ela <ela.yudhanira@scania.com>
2020-04-22 21:43:50 +0200

    Updates

---
+++ precache-manifest.fee5500cef7db615ab62b68fc3e51308.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da32148f73cc819cd6e262c0d90c2fba",
+    "url": "/mainapi/index.html"
+  },
+  {
+    "revision": "b45db496d168c0f70fc5",
+    "url": "/mainapi/static/css/main.59af7252.chunk.css"
+  },
+  {
+    "revision": "0428636493798d07c244",
+    "url": "/mainapi/static/js/2.52b01596.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/mainapi/static/js/2.52b01596.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b45db496d168c0f70fc5",
+    "url": "/mainapi/static/js/main.4102fb0e.chunk.js"
+  },
+  {
+    "revision": "b64c54cc25d8cdcc58e1",
+    "url": "/mainapi/static/js/runtime-main.9899c24b.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.ae9257ba9c3f81c3ad965dd9b1231bc6.js"
+  "/mainapi/precache-manifest.fee5500cef7db615ab62b68fc3e51308.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/mainapi/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
